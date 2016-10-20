a910d7136b5ed33ebf7b90e59cd92d8dbf3a671a
Eric Hansander <ehdr@erichansander.com>
2016-10-20 17:16:12 +0200

    macosx-bash: fix notifications for long running commands
    The [code I used as inspiration][1] runs the notification function
    from `$PS1`, whereas my implementation runs it from `$PROMPT_COMMAND`,
    and from there, `fc -ln -1` refers to the previous command, not the
    currently executing command. Switching to `history 1` solves it.
    
    [1]: http://frantic.im/notify-on-completion

--- macosx-bash/.bash.d/21-macosx-shell-prompt.sh
+++ macosx-bash/.bash.d/21-macosx-shell-prompt.sh
@@ -3,7 +3,7 @@ is_interactive_shell || return
 # inspired by: http://frantic.im/notify-on-completion
 function notify_after_long_running_command() {
     last_status=$?
-    cmd=$(fc -ln -1)
+    cmd=$(history 1 | tr -s ' ' | cut -d ' ' -f 3-)
     # no need to wait for the notify command to finish
     # also, run in (subshell) to prevent '[1]+  Done...' output
     # from shell job control
