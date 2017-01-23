c0fda012c521c416d4a69575a8b6133495d9cde7
Eric Hansander <ehdr@erichansander.com>
2017-01-23 10:32:49 +0100

    macosx-miniconda: Enable unsetting the conda env

--- macosx-miniconda/.bash.d/50-macosx-miniconda.sh
+++ macosx-miniconda/.bash.d/50-macosx-miniconda.sh
@@ -5,11 +5,17 @@ is_interactive_shell || return
 # choose to use the conda ones only by explicitly setting a conda env
 #PATH=$(path_prepend "$PATH" "~/miniconda/bin")
 
+MINICONDA_PATH=~/miniconda3
+
 ## Convenience function for Anaconda miniconda Python environments
 cenv () {
   if [ -z "$*" ]; then
     echo "CONDA_DEFAULT_ENV: ${CONDA_DEFAULT_ENV:-[none]}"
+  elif [ "$*" = "-d" ]; then
+    source "${MINICONDA_PATH}/bin/activate" root
+    PATH=$(path_remove "$PATH" "${MINICONDA_PATH}/bin")
+    echo "discarding ${MINICONDA_PATH}/bin from PATH again"
   else
-    source ~/miniconda/bin/activate $*
+    source "${MINICONDA_PATH}/bin/activate" $*
   fi
 }
