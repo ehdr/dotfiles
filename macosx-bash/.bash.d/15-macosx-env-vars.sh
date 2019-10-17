f5674f1cefcdd10bd2f433890781fbc3a2131699
Eric Hansander <ehdr@erichansander.com>
2019-10-17 21:42:44 +0200

    macosx-bash: remove JAVA_HOME env var

--- macosx-bash/.bash.d/15-macosx-env-vars.sh
+++ macosx-bash/.bash.d/15-macosx-env-vars.sh
@@ -1,10 +1,6 @@
 ## Set up PATH for Xcode utilities
 PATH=$(path_prepend "$PATH" "/Applications/Xcode.app/Contents/Developer/usr/bin")
 
-## Set up Java JDK
-export JAVA_HOME=$(/usr/libexec/java_home)
-
-
 is_interactive_shell || return
 
 export CLICOLOR=1
