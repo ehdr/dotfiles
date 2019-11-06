503a59c1cdaa78823e2f0dab7bd9b26d7fd7fefe
Eric Hansander <ehdr@erichansander.com>
2019-11-06 17:32:48 +0100

    macosx-miniconda: adapt to new version of miniconda

--- macosx-miniconda/.bash.d/50-macosx-miniconda.sh
+++ macosx-miniconda/.bash.d/50-macosx-miniconda.sh
@@ -1,21 +1,11 @@
 is_interactive_shell || return
 
-## Set up PATH for Anaconda miniconda Python distribution
-# Actually, I prefer to have the "system" Python in PATH by default, and
-# choose to use the conda ones only by explicitly setting a conda env
-#PATH=$(path_prepend "$PATH" "~/miniconda/bin")
-
-MINICONDA_PATH=~/miniconda3
-
-## Convenience function for Anaconda miniconda Python environments
 cenv () {
   if [ -z "$*" ]; then
-    echo "CONDA_DEFAULT_ENV: ${CONDA_DEFAULT_ENV:-[none]}"
+    conda info --envs
   elif [ "$*" = "-d" ]; then
-    source "${MINICONDA_PATH}/bin/activate" root
-    PATH=$(path_remove "$PATH" "${MINICONDA_PATH}/bin")
-    echo "discarding ${MINICONDA_PATH}/bin from PATH again"
+    conda deactivate
   else
-    source "${MINICONDA_PATH}/bin/activate" $*
+    conda activate $*
   fi
 }
