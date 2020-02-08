d87ba044e33df90e021234430d5f7fddb15ab8b2
Eric Hansander <ehdr@erichansander.com>
2020-02-08 17:49:52 -0500

    dotfiles: break out df_log and df_error as utility scripts
    To allow calling them from within other scripts.

--- macosx-gnuutils/.bash.d/46-macosx-gnuutils-path.sh
+++ macosx-gnuutils/.bash.d/46-macosx-gnuutils-path.sh
@@ -3,7 +3,7 @@ is_interactive_shell || return
 for util in coreutils findutils grep gawk gnu-sed gnu-tar gnu-indent gnu-which; do
     util_path="/usr/local/opt/${util}/libexec/gnubin"
     if [ ! -d "$util_path" ]; then
-        df_error_message "macosx-gnuutils: failed to find $util - not adding to \$PATH"
+        df_error "macosx-gnuutils: failed to find $util - not adding to \$PATH"
     else
         PATH=$(path_prepend "$PATH" "$util_path")
     fi
--- macosx-tmux/.bash.d/50-macosx-tmux.sh
+++ macosx-tmux/.bash.d/50-macosx-tmux.sh
@@ -2,4 +2,4 @@ is_interactive_shell || return
 
 alias tmux='tmux -f ~/.tmux-macosx.conf'
 
-is_installed reattach-to-user-namespace || df_error_message 'macosx-tmux: warning! reattach-to-user-namespace not installed'
+is_installed reattach-to-user-namespace || df_error 'macosx-tmux: warning! reattach-to-user-namespace not installed'
