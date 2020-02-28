9af1a1990606ff0cd5753c8b617be90cc3f00146
Eric Hansander <ehdr@erichansander.com>
2020-02-27 22:16:16 -0500

    common-python: add penv function to print paths and venv info

--- common-python/.bash.d/50-python.sh
+++ common-python/.bash.d/50-python.sh
@@ -1,6 +1,19 @@
 is_interactive_shell || return
 
-## Initilialize ruby environment
+## Initilialize pyenv
 if is_installed pyenv; then
     eval "$(pyenv init -)"
 fi
+
+function penv {
+    echo "# python"
+    python --version
+    which python
+    which pip
+    echo "# python3"
+    python3 --version
+    which python3
+    which pip3
+    echo
+    echo "VIRTUAL_ENV=${VIRTUAL_ENV:-none}"
+}
