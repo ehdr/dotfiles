bce4686307a75e0dfc1bd7f69c1562d470864c68
Eric Hansander <ehdr@erichansander.com>
2020-02-27 22:14:25 -0500

    macosx-gnuutils: prepend more GNU utils to PATH
    brew installs some under a different path.

--- macosx-gnuutils/.bash.d/46-macosx-gnuutils-path.sh
+++ macosx-gnuutils/.bash.d/46-macosx-gnuutils-path.sh
@@ -8,3 +8,12 @@ for util in coreutils findutils grep gawk gnu-sed gnu-tar gnu-indent gnu-which; 
         PATH=$(path_prepend "$PATH" "$util_path")
     fi
 done
+
+for util in gnu-getopt; do
+    util_path="/usr/local/opt/${util}/bin"
+    if [ ! -d "$util_path" ]; then
+        df_error "macosx-gnuutils: failed to find ${util} - not adding to \$PATH"
+    else
+        PATH=$(path_prepend "$PATH" "$util_path")
+    fi
+done
