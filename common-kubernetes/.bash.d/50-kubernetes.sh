f37aac78701b69973b4b97ce81ef00a92e722366
Eric Hansander <ehdr@erichansander.com>
2021-09-13 10:43:31 -0400

    common-kubernetes: add various convenience aliases/functions

--- common-kubernetes/.bash.d/50-kubernetes.sh
+++ common-kubernetes/.bash.d/50-kubernetes.sh
@@ -3,3 +3,18 @@ is_interactive_shell || return
 alias k=kubectl
 alias kn=kubens
 alias kx=kubectx
+alias mk='minikube kubectl --'
+
+function kgpg { kubectl get pods --sort-by=.metadata.creationTimestamp | egrep ${@:-''}; }
+function kgpg1p {
+    result=$(kgpg $*)
+    if [ -z "$result" -o "$(echo "$result" | grep -c '^')" != "1" ]; then
+        >&2 echo "Need exactly one match, but got:"
+        >&2 echo
+        >&2 echo "$result"
+        return 1;
+    fi
+    echo "$result" | cut -d' ' -f1
+}
+function klg { pod=$(kgpg1p $*); (set -x; kubectl logs "$pod"; ) }
+function kesg { pod=$(kgpg1p $*); (set -x; kubectl exec -it "$pod" sh; ) }
