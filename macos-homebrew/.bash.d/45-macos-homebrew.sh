e7c2265b7dcd5fa602a4fc83fac039d7eee42f18
Eric Hansander <ehdr@erichansander.com>
2021-12-08 10:17:09 -0500

    macos-homebrew: disable auto updates

--- macos-homebrew/.bash.d/45-macos-homebrew.sh
+++ macos-homebrew/.bash.d/45-macos-homebrew.sh
@@ -7,3 +7,5 @@ is_interactive_shell || return
 if is_installed brew && [ -f $(brew --prefix)/etc/bash_completion ] && ! shopt -oq posix; then
   source $(brew --prefix)/etc/bash_completion
 fi
+
+export HOMEBREW_NO_AUTO_UPDATE=1
