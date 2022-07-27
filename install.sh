36be2b2a13c6871fc2a5712c87ba7699727ceca9
Eric Hansander <ehdr@erichansander.com>
2022-07-27 22:21:27 +0200

    install.sh: properly handle directory names that contain spaces

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "Installing packages: $packages"
 # create all directories we will put files into (othewise `stow` will
 # symlink the whole directory, not just the files within it):
 echo "Making directories..."
-for d in $(find $packages -type d | grep -v '\.git' | grep / | cut -d'/' -f2- | sort -u | egrep -v '^\.$|^$'); do
+find $packages -type d | grep -v '\.git' | grep / | cut -d'/' -f2- | sort -u | egrep -v '^\.$|^$' | while IFS= read -r d; do
     echo "- ~/$d"
     mkdir -p ~/$d
 done
