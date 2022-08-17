35e02fac99006dc8c0358be7248d5d8f8901c9b8
Eric Hansander <ehdr@erichansander.com>
2022-08-18 00:09:26 +0200

    common-tmux: add alias to reattach to running session by default

--- common-tmux/.bash.d/50-tmux.sh
+++ common-tmux/.bash.d/50-tmux.sh
@@ -5,3 +5,5 @@ is_installed tmux || return
 if [ ! -d ~/.tmux/plugins/tpm ]; then
     df_log "common-tmux: tmux tpm is not installed - run ~/dotscripts/init-tmux"
 fi
+
+alias tmux0='tmux new -A -s 0'
