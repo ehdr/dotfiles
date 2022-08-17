1e3e0dc40c4ebbc93b45486ad7eec5c51341140b
Eric Hansander <ehdr@erichansander.com>
2022-08-18 00:10:04 +0200

    macos-homebrew: update path to bash completion scripts

--- macos-homebrew/.bash.d/45-macos-homebrew.sh
+++ macos-homebrew/.bash.d/45-macos-homebrew.sh
@@ -4,8 +4,8 @@ fi
 
 is_interactive_shell || return
 
-if is_installed brew && [ -f $(brew --prefix)/etc/bash_completion ] && ! shopt -oq posix; then
-  source $(brew --prefix)/etc/bash_completion
+if is_installed brew && [ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ] && ! shopt -oq posix; then
+  source "$(brew --prefix)/etc/profile.d/bash_completion.sh"
 fi
 
 export HOMEBREW_NO_AUTO_UPDATE=1
