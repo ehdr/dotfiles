d9eb9e3164a90b444b47b60519c848cada3b7910
Eric Hansander <ehdr@erichansander.com>
2022-08-18 00:11:16 +0200

    macos-homebrew: add script to perform a safe brew upgrade
    It does it in a zsh without loading any user specific configuration, to prevent custom configuration from intergering with upgrading and building packages.

---
+++ macos-homebrew/dotscripts/brew-upgrade.zsh
@@ -0,0 +1,53 @@
+#!/usr/bin/env -i zsh -f
+# ^ starts zsh without loading personal config files or environment variables,
+# to prevent things like gnu coreutilities from interfering with upgrades.
+
+set -o errexit
+set -o pipefail
+
+if [ -x /opt/homebrew/bin/brew ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+function echo_and_run () {
+  echo
+  echo "+$@"
+  $@
+}
+
+echo_and_run brew update
+echo_and_run brew doctor
+echo_and_run brew update
+
+to_upgrade=$(brew outdated)
+if [ -z "$to_upgrade" ]; then
+  exit 0
+fi
+
+echo
+if ! read -qs 'i?Continue upgrade? (y/N) '; then
+  echo
+  exit 0
+fi
+
+echo_and_run brew upgrade
+echo_and_run brew cleanup
+
+to_autoremove=$(brew autoremove --dry-run)
+if [ -z "$to_autoremove" ]; then
+  exit 0
+fi
+
+echo
+echo $to_autoremove
+echo
+# `read -q` returns "zero only if the character was ‘y’ or ‘Y’", see:
+# http://zsh.sourceforge.net/Doc/Release/Shell-Builtin-Commands.html#index-read
+if read -qs 'i?Remove them by running `brew autoremove`? (y/N) '; then
+  echo_and_run brew autoremove
+  echo_and_run brew cleanup
+fi
+
+echo_and_run brew doctor
+
+echo
