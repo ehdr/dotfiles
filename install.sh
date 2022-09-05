c7d2a29dfce1998ece8638c777d2c597e51e6e40
Eric Hansander <ehdr@erichansander.com>
2022-09-05 13:19:43 +0200

    dotfiles: use stow --no-folding instead of home-grown solution
    Context: https://news.ycombinator.com/item?id=7927930

--- install.sh
+++ install.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
-set -e
+set -eu
 set -o pipefail
 
 packages="common-* $*"
 echo "Installing packages: $packages"
 
-# create all directories we will put files into (othewise `stow` will
-# symlink the whole directory, not just the files within it):
-echo "Making directories..."
-find $packages -type d | grep -v '\.git' | grep / | cut -d'/' -f2- | sort -u | egrep -v '^\.$|^$' | while IFS= read -r d; do
-    echo "- ~/$d"
-    mkdir -p ~/$d
-done
-echo "done."
-
 echo "Stowing..."
-for p in ${packages}; do stow -Rv ${p}; done
+for p in ${packages}; do stow --no-folding -Rv ${p}; done
 echo "done."
