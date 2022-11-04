a03feae92df99e3f5da19bccfdc302a0eb05be55
Eric Hansander <ehdr@erichansander.com>
2022-11-04 23:48:53 +0100

    macos-homebrew: add man paths for GNU utils

--- macos-homebrew/.bash.d/46-macos-homebrew-path-overrides.sh
+++ macos-homebrew/.bash.d/46-macos-homebrew-path-overrides.sh
@@ -1,20 +1,48 @@
 is_interactive_shell || return
 
 for util in coreutils findutils grep gawk gnu-sed gnu-tar gnu-indent gnu-which; do
-    util_path="$(brew --prefix)/opt/${util}/libexec/gnubin"
-    if [ ! -d "$util_path" ]; then
-        df_error "macos-gnuutils: failed to find $util - not adding to \$PATH"
+    util_path="$(brew --prefix)/opt/${util}/libexec"
+    bin_dir="$util_path/gnubin"
+    if [ ! -d "$bin_dir" ]; then
+        df_error "macos-homebrew: failed to find $util - not adding to \$PATH"
     else
-        PATH=$(path_prepend "$PATH" "$util_path")
+        PATH=$(path_prepend "$PATH" "$bin_dir")
+    fi
+
+    man_dir="$util_path/gnuman"
+    if [ -d "$man_dir" ]; then
+        MANPATH=$(path_prepend "$MANPATH" "$man_dir")
     fi
 done
 
-for util in gnu-getopt curl; do
-    util_path="$(brew --prefix)/opt/${util}/bin"
-    if [ ! -d "$util_path" ]; then
-        df_error "macos-gnuutils: failed to find ${util} - not adding to \$PATH"
+for util in man-db; do
+    util_path="$(brew --prefix)/opt/${util}/libexec"
+    bin_dir="$util_path/bin"
+    if [ ! -d "$bin_dir" ]; then
+        df_error "macos-homebrew: failed to find $util - not adding to \$PATH"
     else
-        PATH=$(path_prepend "$PATH" "$util_path")
+        PATH=$(path_prepend "$PATH" "$bin_dir")
+    fi
+
+    man_dir="$util_path/man"
+    if [ -d "$man_dir" ]; then
+        MANPATH=$(path_prepend "$MANPATH" "$man_dir")
     fi
 done
-unset util util_path
+
+
+for util in curl gnu-getopt; do
+    util_path="$(brew --prefix)/opt/${util}"
+    bin_dir="$util_path/bin"
+    if [ ! -d "$bin_dir" ]; then
+        df_error "macos-homebrew: failed to find ${util} - not adding to \$PATH"
+    else
+        PATH=$(path_prepend "$PATH" "$bin_dir")
+    fi
+
+    man_dir="$util_path/share/man"
+    if [ -d "$man_dir" ]; then
+        MANPATH=$(path_prepend "$MANPATH" "$man_dir")
+    fi
+done
+unset util util_path bin_dir man_dir
