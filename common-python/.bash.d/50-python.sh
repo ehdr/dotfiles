3467046cb472fb6d81214ea691885d514bbd5edc
Eric Hansander <ehdr@erichansander.com>
2025-04-29 17:18:38 +0200

    common-python: Fix path prepending

--- common-python/.bash.d/50-python.sh
+++ common-python/.bash.d/50-python.sh
@@ -1,7 +1,7 @@
 ## Initilialize pyenv
 if is_installed pyenv; then
     export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
+    PATH=$(path_prepend "$PATH" "$PYENV_ROOT/bin")
 fi
 
 
