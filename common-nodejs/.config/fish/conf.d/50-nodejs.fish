f7ff9d560c656df00968f9456cd157ccdea9d1af
Eric Hansander <ehdr@erichansander.com>
2025-09-19 22:35:05 +0200

    common-nodejs: fix nvm setup

--- common-nodejs/.config/fish/conf.d/50-nodejs.fish
+++
@@ -1,7 +0,0 @@
-is_installed nvm; or return
-
-set -gx NVM_DIR (test -z "$XDG_CONFIG_HOME"; and printf %s "$HOME/.nvm"; or printf %s "$XDG_CONFIG_HOME/nvm")
-
-if test -s "$NVM_DIR/nvm.sh"
-    source "$NVM_DIR/nvm.sh"
-end
