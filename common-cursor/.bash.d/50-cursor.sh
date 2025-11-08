e3b767e9707e0531e9cafb03efdc2a3eb52ab6e8
Eric Hansander <ehdr@erichansander.com>
2025-11-08 14:09:11 +0100

    Add c. alias

---
+++ common-cursor/.bash.d/50-cursor.sh
@@ -0,0 +1,4 @@
+is_interactive_shell || return
+is_installed cursor || return
+
+alias c.='cursor .'
---
+++ common-cursor/.config/fish/conf.d/50-cursor.fish
@@ -0,0 +1,4 @@
+is_interactive_shell; or return
+is_installed cursor; or return
+
+abbr -a c. 'cursor .'
