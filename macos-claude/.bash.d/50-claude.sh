838e7a0cb762c3366351cf9df9405855243df1bc
Eric Hansander <ehdr@erichansander.com>
2025-11-08 14:10:32 +0100

    Add macos-claude

---
+++ macos-claude/.bash.d/50-claude.sh
@@ -0,0 +1,3 @@
+is_interactive_shell || return
+
+export CLAUDE_BASH_MAINTAIN_PROJECT_WORKING_DIR=1
---
+++ macos-claude/.config/fish/conf.d/50-claude.fish
@@ -0,0 +1,5 @@
+is_interactive_shell; or return
+
+abbr -a cc 'cbash claude'
+
+set -gx CLAUDE_BASH_MAINTAIN_PROJECT_WORKING_DIR 1
