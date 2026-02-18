f8fce2cb1e27dc183e32439b6e7de71f8508ef27
Eric Hansander <ehdr@erichansander.com>
2026-02-18 21:02:38 +0100

    common-nodejs: switch from nvm to fnm

--- common-nodejs/.bash.d/50-nodejs.sh
+++ common-nodejs/.bash.d/50-nodejs.sh
@@ -1,6 +1,5 @@
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+is_installed fnm && eval "$(fnm env --shell bash)"
 
 is_interactive_shell || return
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+is_installed fnm && eval "$(fnm env --use-on-cd --shell bash)"
---
+++ common-nodejs/.config/fish/conf.d/50-nodejs.fish
@@ -0,0 +1,5 @@
+is_installed fnm; and fnm env --shell fish | source
+
+is_interactive_shell; or return
+
+is_installed fnm; and fnm env --use-on-cd --shell fish | source
